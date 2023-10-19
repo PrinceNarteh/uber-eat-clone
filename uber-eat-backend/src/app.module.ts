3f9e4678a5984d29c8c8a278cadac87e328c3023
Prince Narteh <prinarteh@gmail.com>
2023-10-19 05:58:06 +0000

    feat: add database

--- uber-eat-backend/src/app.module.ts
+++ uber-eat-backend/src/app.module.ts
@@ -3,9 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {GraphQLModule} from "@nestjs/graphql";
 import {ApolloDriver, ApolloDriverConfig} from "@nestjs/apollo";
+import {TypeOrmModule} from "@nestjs/typeorm";
 
 @Module({
-  imports: [GraphQLModule.forRoot<ApolloDriverConfig>({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "postgres",
+      password: "root",
+      synchronize: true,
+      logging:true,
+      database: "uber_eat_clone"
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
     driver: ApolloDriver,
     autoSchemaFile: true
   })],
